c1dfbb8e14ad7cfd49dea6f309de41e659f6084c
odoolargotek <67128587+odoolargotek@users.noreply.github.com>
2025-12-13 19:39:39 -0400

    excel de carga de roster

--- global_brigades/__manifest__.py
+++ global_brigades/__manifest__.py
@@ -11,7 +11,7 @@
         "base",
         "contacts",
     ],
-        "data": [
+    "data": [
         "data/sequence.xml",
         "security/security.xml",
         "security/ir.model.access.csv",
@@ -23,12 +23,12 @@
         "views/brigade_hotel_booking_views.xml",
         "views/passenger_list_wizard_views.xml",
         "views/brigade_transport_views.xml",
-        'views/community_views.xml',
-        'views/brigade_report_views.xml',
+        "views/community_views.xml",
+        "views/brigade_report_views.xml",
         "views/brigade_extended_report_views.xml",
+        "views/roster_import_wizard_views.xml",   # <-- NUEVO
         "data/gb_activity_tag_data.xml",
     ],
-
     "application": True,
     "installable": True,
 }
