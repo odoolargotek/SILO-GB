2d75923793aaa2d3721923243c9f442b6a255faf
odoolargotek <67128587+odoolargotek@users.noreply.github.com>
2026-03-31 06:32:15 -0400

    aumento de la accion

--- global_brigades/__manifest__.py
+++ global_brigades/__manifest__.py
@@ -43,6 +43,7 @@ Logistics and volunteer management for Global Brigades:
         "views/brigade_transport_report_views.xml",
         "views/sa_notification_brigade_view.xml",
         "views/brigade_detailed_report_views.xml",
+        "data/brigade_detailed_report_action.xml",
 
 
 
